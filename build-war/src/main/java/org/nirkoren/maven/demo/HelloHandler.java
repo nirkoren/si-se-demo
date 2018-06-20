10a4685193741c29ceff775763819b94ab0e56ac
PaddieG <40421793+PaddieG@users.noreply.github.com>
2018-06-20 12:35:27 +0200

    Update HelloHandler.java

--- build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
@@ -3,7 +3,7 @@ package org.nirkoren.maven.demo;
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Dear SI-SE members, friends and Romans, What do you think about this conference?";
+		String hello = "Dear SI-SE members and Romans, What do you think about this conference?";
 		return hello;
 	}
 
